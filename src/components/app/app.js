53928925bc97a381f884f08c3f5bcb4099623578
jjspace <jjrouzer@yahoo.com>
2019-07-03 11:50:58 -0400

    feat(series-list): start using state, add series button

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,23 +4,38 @@ import Header from '../header/header';
 import SeriesList from '../series-list/series-list';
 import './app.scss';
 
-const seriesList = [
+const demoSeriesList = [
   { name: 'series 1' },
   { name: 'series 2' },
 ]
 
-function App(props) {
-  return (
-    <div className='App'>
-      <Header />
-      <div className='wrapper'>
-        <div className='info-block'>
-          Bulk Cal is a tool to quickly create series of related events to be added to your calendar. Start by clicking Add Series below.
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      seriesList: demoSeriesList,
+    }
+  }
+
+  addSeries = () => {
+    this.setState((prevState) => ({seriesList: [...prevState.seriesList, {name: 'randome str'}]}))
+  }
+
+  render() {
+    const {seriesList} = this.state;
+    return (
+      <div className='App'>
+        <Header />
+        <div className='wrapper'>
+          <div className='info-block'>
+            Bulk Cal is a tool to quickly create series of related events to be added to your calendar. Start by clicking Add Series below.
+          </div>
+          <SeriesList seriesList={seriesList} addSeries={this.addSeries} />
         </div>
-        <SeriesList seriesList={seriesList} />
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default hot(module)(App);
--- src/components/series-list/series-list.js
+++ src/components/series-list/series-list.js
@@ -3,23 +3,25 @@ import PropTypes from 'prop-types';
 import Series from '../series/series';
 import './series-list.scss';
 
-function SeriesList({ seriesList }) {
+function SeriesList({ seriesList, addSeries }) {
   return (
     <div className='series-list'>
       {seriesList.map((series, i) => {
         return <Series key={series.name + i} series={series} />
       })}
-      <button>+</button>
+      <button onClick={addSeries} type='button'>+</button>
     </div>
   )
 }
 
 SeriesList.defaultProps = {
   seriesList: [],
+  addSeries: () => {},
 }
 
 SeriesList.propTypes = {
   seriesList: PropTypes.array,
+  addSeries: PropTypes.func,
 }
 
 export default SeriesList;
